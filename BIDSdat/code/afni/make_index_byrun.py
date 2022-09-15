7dfa2bf4e6331e78145a0bf925455093883a43bc
bfuchs18 <bari.fuchs@gmail.com>
2022-09-15 17:36:31 -0400

    rework to reference onsets instead of censorsum

--- BIDSdat/code/afni/make_index_byrun.py
+++ BIDSdat/code/afni/make_index_byrun.py
@@ -1,12 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-This script was created to (1) count the number of subjects to be included in analyses 
-based on motion threshold and (2) generate subject index files for AFNI analyses using 
-task-foodcue..censorsummary_$censorcritera.tsv files (output of 4_create_censor_files.py). 
-Index files will list subject IDs that should be included in analyses, based on the desired motion threshold. 
-Index files be generated for the whole group, and can be generated for risk groups using the -byrisk input arg
-
 Written by Bari Fuchs in Spring 2022
 
 Copyright (C) 20120 Bari Fuchs
@@ -32,16 +26,10 @@ or raw data configurations.
 """
 
 #set up packages    
-from email import header
-from pickle import TRUE
-from aem import con
-import numpy as np
 import pandas as pd
 import os
-import sys, argparse
-#from scipy.stats import norm
-from scipy.stats.morestats import shapiro
 from pathlib import Path
+import re
 
 ##############################################################################
 ####                                                                      ####
@@ -49,16 +37,13 @@ from pathlib import Path
 ####                                                                      ####
 ##############################################################################
 
-# to overwrite onsets of all input IDs, specify in terminal format like:
-#-f
+def _represents_float(s):
+    try: 
+        float(s)
+        return True
+    except ValueError:
+        return False
 
-#input arguments setup
-parser=argparse.ArgumentParser()
-parser.add_argument('--censorsumfile', '-c', help='name of censor summary file (e.g., task-foodcue_censorsummary_fd-1.0.tsv', type=str)
-parser.add_argument('--pthresh_r', '-r', help='threshold for censoring runs based on percent of TRs censored across the whole run', type=int)
-parser.add_argument('--pthresh_b', '-b', help='threshold for censoring runs based on percent of TRs censored in blocks of interest', type=int)
-parser.add_argument('--generateIndex', '-g', help='Generate index files', action='store_true')
-args=parser.parse_args()
 
 ##############################################################################
 ####                                                                      ####
@@ -66,121 +51,134 @@ args=parser.parse_args()
 ####                                                                      ####
 ##############################################################################
 
-# get script location
-script_path = Path(__file__).parent.resolve()
+def gen_index_byrun(onset_dir, nruns, preproc_path = False):
 
-# change directory to base directory (BIDSdat) and get path
-os.chdir(script_path)
-os.chdir('../../..')
-pardata_directory = Path(os.getcwd())
-#bids_directory = Path(os.getcwd())
+    """Function to generate an index file for AFNI analyses that lists subjects with a
+    sufficient number of good runs (nruns) to be included in analyses. 
+    Index files be generated for the whole group, and can be generated for risk groups using the -byrisk input arg
+    Inputs:
+        onset_dir (string): name of directory where censored onsetfiles are located (e.g., 'fd-1.0_cpt_b20')
+        nruns (int): minimum number of "good" runs that a subject needs to be included in analyses
+        minblockTR (int): threshold for censoring blocks. This is the minimum number of uncensored TRs for a block to be included
+    Outputs:
+        onsetfile_dat: 1 onset dataframe per condition, exported as a csv. Onsets for blocks with motion that exceeds
+            minblockTR will replaced with '*'
+    """
 
-#set specific paths
-bids_path = Path(pardata_directory).joinpath('BIDSdat')
-bids_fmriprep_path = Path(pardata_directory).joinpath('BIDSdat/derivatives/preprocessed/fmriprep')
-database_path = Path(pardata_directory).joinpath('Databases')
+    # set base_directory
+    if preproc_path is False:
 
-# set framewise displacement and std_dvar threshold variables
-censorsummary_file = args.censorsumfile
+        # get script location
+        script_path = Path(__file__).parent.resolve()
 
-# Import censor summary database for TR censor criteria
-censor_summary_path = Path(bids_fmriprep_path).joinpath( str(censorsummary_file))
+        # change directory to base directory (BIDSdat) and get path
+        os.chdir(script_path)
+        os.chdir('../../..')
+        base_directory = Path(os.getcwd())
 
-# if database exists
-if censor_summary_path.is_file():
-    # import database
-    censor_summary_allPar = pd.read_csv(str(censor_summary_path), sep = '\t')
-else:
-    print("censor summary file does not exist")
+        # set path to onset files
+        bids_onset_path = Path(base_directory).joinpath('derivatives/preprocessed/foodcue_onsetfiles')
 
-###### get list of subjects with N acceptable runs ######
+        # change directory to Databases and get path
+        os.chdir('../Databases')
+        database_path = Path(os.getcwd())
 
-# subset data to remove sub 999 
-censor_summary_allPar = censor_summary_allPar[censor_summary_allPar["sub"] != 999]
+    elif isinstance(preproc_path, str):
+        # make input string a path
+        preprocessed_directory = Path(preproc_path)
 
-# subset data to remove runs with less than RuncensorCriteria acceptable TRs
+        #set specific paths
+        bids_onset_path = Path(preprocessed_directory).joinpath('foodcue_onsetfiles')
 
-# if censoring based on blocks of interest
-if (args.pthresh_b is not None) and (args.pthresh_r is None):
-    p_thresh_block = args.pthresh_b
-    good_runs = censor_summary_allPar[censor_summary_allPar["p_censor_interest"] < p_thresh_block]
-    exclude_string = "b" + str(p_thresh_block)
-# if censoring based on total run and blocks of interest
-elif (args.pthresh_b is not None) and (args.pthresh_r is not None):
-    p_thresh_block = args.pthresh_b
-    p_thresh_run = args.pthresh_r
-    exclude_string = "r" + str(p_thresh_run) + "b" + str(p_thresh_block)
-    #good_runs = censor_summary_allPar[censor_summary_allPar["p_censor"] < p_thresh_run]
-# if censoring based on total run 
-elif (args.pthresh_b is None) and (args.pthresh_r is not None):
-    p_thresh_run = args.pthresh_r
-    good_runs = censor_summary_allPar[censor_summary_allPar["p_censor"] < p_thresh_run]
-    exclude_string = "r" + str(p_thresh_run)
+        # change directory to Databases and get path
+        os.chdir('../Databases')
+        database_path = Path(os.getcwd())
 
-# get number of good runs per subject
-n_good_persub = good_runs.groupby('sub').size().reset_index(name='n_good_runs')
+    else: 
+        print("preproc_path must be string")
+        raise Exception()
 
-# Subset data to include subjects with >2 good runs
-sub_include_3runs = n_good_persub[n_good_persub["n_good_runs"] >= 3]
-sub_include_2runs = n_good_persub[n_good_persub["n_good_runs"] >= 2]
+    # Set onset directory
+    onset_directory = Path(bids_onset_path).joinpath( str(onset_dir))
 
-# rename sub column to id to facilitate merge
-sub_include_3runs=sub_include_3runs.rename(columns = {'sub':'id'})
-sub_include_2runs=sub_include_2runs.rename(columns = {'sub':'id'})
+    # check onset directory exists
+    if onset_directory.is_dir() is False:
+        print("onset directory does not exist")
+        raise Exception
 
-# Add risk information to sub_include
-anthro_df = pd.read_spss(Path(database_path).joinpath('anthro_data.sav'))
-sub_include_3runs = pd.merge(sub_include_3runs,anthro_df[['id','risk_status_mom']],on='id', how='left')
-sub_include_2runs = pd.merge(sub_include_2runs,anthro_df[['id','risk_status_mom']],on='id', how='left')
+    ###########################################
+    #### Make list of IDs with enough runs ####
+    ###########################################
 
-# subset based on low and high risk
-high_risk_df_3runs = sub_include_3runs[sub_include_3runs["risk_status_mom"] == "High Risk"]
-low_risk_df_3runs = sub_include_3runs[sub_include_3runs["risk_status_mom"] == "Low Risk"]
+    # get list of subjects with onset files
 
-high_risk_df_2runs = sub_include_2runs[sub_include_2runs["risk_status_mom"] == "High Risk"]
-low_risk_df_2runs = sub_include_2runs[sub_include_2runs["risk_status_mom"] == "Low Risk"]
+    onsetfiles = []
+    for file in onset_directory.rglob('*'):  # loop recursively over all subdirectories
+        onsetfiles.append(file.name)
 
-# Get lists of subjects with leading zeros
-all_3runs = [str(sub).zfill(3) for sub in sub_include_3runs['id']]
-highrisk_3runs = [str(sub).zfill(3) for sub in high_risk_df_3runs['id']]
-lowrisk_3runs = [str(sub).zfill(3) for sub in low_risk_df_3runs['id']]
+    onsetfiles = [file.stem for file in onset_directory.rglob('*')]
+    subs = [re.split('_|-',file)[1] for file in onsetfiles]
+    subs = list(set(subs)) # get unique list of subjects
+    subs = (sorted(subs))
 
-all_2runs = [str(sub).zfill(3) for sub in sub_include_2runs['id']]
-highrisk_2runs = [str(sub).zfill(3) for sub in high_risk_df_2runs['id']]
-lowrisk_2runs = [str(sub).zfill(3) for sub in low_risk_df_2runs['id']]
+    # get list of subjects with >= nruns
+    subs_to_include = []
+    for sub in subs:
 
+        # read in HighLarge onset file -- the same runs will be censored from every condition, so only need to assess 1 onset file
+        HL_onset = Path(onset_directory).joinpath('sub-' + str(sub) + '_HighLarge-AFNIonsets.txt')
+        HL_file = pd.read_csv(HL_onset, sep = '\t', encoding = 'utf-8-sig', engine='python', header=None)
 
-###########################################
-#### Output summary numbers to terminal ###
-###########################################
+        nruns_sub = 0
+        # Loop through rows in onset file (row i corresponds to run i+1)
+        for i in range(len(HL_file)):
+            if _represents_float(HL_file[0].iloc[i]):
+                nruns_sub = nruns_sub + 1
+        
+        if nruns_sub >= nruns:
+            subs_to_include.append(sub)
 
-print("N evaluated:", censor_summary_allPar['sub'].nunique())
-print("")
-print("N with at least 3 good runs:", len(all_3runs))
-print("N high risk with at least 3 good runs:", len(highrisk_3runs))
-print("N low risk with at least 3 good runs:", len(lowrisk_3runs))
-print("")
-print("N with at least 2 good runs:", len(all_2runs))
-print("N high risk with at least 2 good runs:", len(highrisk_2runs))
-print("N low risk with at least 2 good runs:", len(lowrisk_2runs))
+    ##################################
+    #### Make lists by risk group ####
+    ##################################
 
-###########################################
-#### Generate index files for each list ###
-###########################################
+    # make dataframe from subs_to_include list
+    sub_include_df = pd.DataFrame({'id':subs_to_include})
 
-for name in (['all_3runs', 'highrisk_3runs', 'lowrisk_3runs', 'all_2runs', 'highrisk_2runs', 'lowrisk_2runs']):
-    print(name)
-    # get group name
-    #group = name.split('_', 1)[0]
-    group = name
-    # get list of IDs 
-    list = globals()[name]
-    # define output path
-    censor_string = censorsummary_file.rsplit('summary_',1)[1][0:-4]
-    file = bids_path.joinpath('derivatives/analyses/FoodCue-fmri/Level2GLM/Activation_Univariate/ses-1/index-' + group + '_' + str(censor_string) + "_" + str(exclude_string) + '.txt')
-    # write to file
-    with open(file, 'w') as indexFile:
-        joined_list = "  ".join(list)
-        print(joined_list , file = indexFile)
+    # Import and format dataframe with risk info
+    anthro_df = pd.read_spss(Path(database_path).joinpath('anthro_data.sav'))
+    anthro_df['id']= anthro_df['id'].astype(int) #change to integer to remove decimals
+    anthro_df['id']= anthro_df['id'].astype(str) #change to string
+    anthro_df['id'] = anthro_df['id'].str.zfill(3) #add leading zeros
+
+    # Merge risk into into sub_include_df
+    sub_include_df = pd.merge(sub_include_df,anthro_df[['id','risk_status_mom']],on='id', how='left')
+
+    # subset based on low and high risk
+    high_risk_df = sub_include_df[sub_include_df["risk_status_mom"] == "High Risk"]
+    low_risk_df = sub_include_df[sub_include_df["risk_status_mom"] == "Low Risk"]
+
+    # Convert dataframes to lists
+    all = [str(sub) for sub in sub_include_df['id']]
+    highrisk = [str(sub) for sub in high_risk_df['id']]
+    lowrisk = [str(sub) for sub in low_risk_df['id']]
+
+    ###########################################
+    #### Generate index files for each list ###
+    ###########################################
+
+    for group in (['all', 'highrisk', 'lowrisk']):
+
+        # get list of IDs 
+        list = globals()[group]
+
+        # define output path
+        censor_string = str(onset_dir)
+
+        file = base_directory.joinpath('derivatives/analyses/FoodCue-fmri/Level2GLM/Activation_Univariate/ses-1/index-' + group + '_' + str(censor_string) + '_' + str(nruns) + 'runs.txt')
+        # write to file
+        with open(file, 'w') as indexFile:
+            joined_list = "  ".join(list)
+            print(joined_list , file = indexFile)
 
 
