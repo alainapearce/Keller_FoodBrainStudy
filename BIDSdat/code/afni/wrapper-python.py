886ad2ed31a1975a0db0d39d93d810dc8429c785
bfuchs18 <bari.fuchs@gmail.com>
2022-09-16 16:12:20 -0400

    initial commit

---
+++ BIDSdat/code/afni/wrapper-python.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+This script was created to run python functions related to generating onsets and censorfiles 
+that will be used for analyses with AFNI. 
+
+Written by Bari Fuchs in August 2022
+
+Copyright (C) 20120 Bari Fuchs
+
+     This program is free software: you can redistribute it and/or modify
+     it under the terms of the GNU General Public License as published by
+     the Free Software Foundation, either version 3 of the License, or
+     (at your option) any later version.
+
+     This program is distributed in the hope that it will be useful,
+     but WITHOUT ANY WARRANTY; without even the implied warranty of
+     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+     GNU General Public License for more details.
+
+     You should have received a copy of the GNU General Public License
+     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+     
+This script is not guaranteed to work for new data or under new directory 
+configurations, however, it should work if no changes are made to directories
+or raw data configurations.
+
+@author: baf44
+"""
+
+#set up packages    
+
+# import data processing functions
+import make_index_byrun
+import make_index_byblock
+
+
+###################################
+### call functions ###
+###################################
+
+make_index_byrun.gen_index_byrun(onset_dir = 'fd-0.9_b20', nruns = 3, preproc_path = False)
+
+make_index_byblock.gen_index_byblock(onset_dir = 'fd-0.9_by-block-7', nblocks = 3, preproc_path = False)
