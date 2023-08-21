80520c248903fc0f2501a258ab4b983e395c85d9
bfuchs18 <bari.fuchs@gmail.com>
2023-08-21 17:27:40 -0400

    remove get_avg_fd
    code for prep_avg_motion is now in code/foodcue_proc/p6_calc_avg_motion. This will be called as part of foodcue processing, rather than for a specific project

--- BIDSdat/code/afni/groupanalyses_paper1/wrapper-python.py
+++ BIDSdat/code/afni/groupanalyses_paper1/wrapper-python.py
@@ -72,14 +72,6 @@ subs = list(set([item[4:7] for item in confound_subs_clean]))
 
 prep_index_byrun.gen_index_byrun(onset_dir = 'fd-0.9_b20', nruns = 3, preproc_path = False)
 
-# Calculate average motion variables -- included in covariate table by make_3dttest_covTable()
-for sub in subs:
-
-     try:
-           prep_avg_motion.get_avg_fd(par_id = sub, preproc_path=False, overwrite=False)
-     except:
-            print("Discontinuing calc_avg_motion() for sub_" + sub)
-
 ## Generate covariate table
 prep_3dttest_covTable.gen_dataframe()
 
