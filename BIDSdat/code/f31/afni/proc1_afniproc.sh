11c1cb54d9ce27bb9525f08c2e7f0525a73ca4d6
bfuchs18 <bari.fuchs@gmail.com>
2024-06-24 16:54:17 -0400

    execute proc script, debug — remove extra ‘\’

--- BIDSdat/code/f31/afni/proc1_afniproc.sh
+++ BIDSdat/code/f31/afni/proc1_afniproc.sh
@@ -14,21 +14,22 @@ set bidsdir = $cwd
 #set input argument 1 to variable 'parID' and make sure it has leading zeros
 set parID = "sub-$1"
 
-# set onset directory
-set onsetDir = $bidsdir/derivatives/preprocessed/f31_onsetfiles/
-
 # set subject's fmriprep session 1 directory
 set fmriprep_sesDir = $bidsdir/derivatives/preprocessed/fmriprep/${parID}/ses-1/
 
-# set path to censor and regressor files
+# set path to directory with onsets, censor and regressor files
 set pythonproc_dir = $bidsdir/derivatives/preprocessed/f31_python_proc/
+
+# set onset directory
+set onsetDir = $pythonproc_dir/onsets/
 
 
 ###################### AFNI: afni_proc.py  ###########################
 
 # run afni proc with and without global_signal regressor (gsr)
 
-foreach str ("gsr" "nogsr")
+#foreach str (gsr nogsr)
+foreach str (gsr)
 
     if ("$str" == "gsr") then
         set reg_fil = ${pythonproc_dir}/${parID}_f31nuireg-gsr-noheader.tsv
@@ -46,6 +47,7 @@ foreach str ("gsr" "nogsr")
     # move to output directory
     cd $outdir
 
+    # generate afniproc script
     afni_proc.py -subj_id ${parID} -script proc_${parID}   \
         -blocks blur scale regress                            \
         -dsets ${fmriprep_sesDir}/func/${parID}_ses-1_task-foodcue_run-?_space-MNIPediatricAsym_cohort-3_res-1_desc-preproc_bold.nii.gz                                              \
@@ -60,9 +62,10 @@ foreach str ("gsr" "nogsr")
         -regress_censor_extern ${pythonproc_dir}/${parID}_f31censor_rmsd-0.3_c-ba.1D    \
         -regress_bandpass         0.01 0.1                             				\
         -regress_opts_3dD                                                                           \
-            -jobs 2                                                                                 \                                                                   \
+            -jobs 2                                                                                 \
         -regress_no_fitts                                                                      \
         -regress_make_ideal_sum sum_ideal.1D                                                        \
         -regress_run_clustsim no
 
-    
+    # execute script
+    tcsh -xef proc_sub-001 |& tee output.proc_sub-001
